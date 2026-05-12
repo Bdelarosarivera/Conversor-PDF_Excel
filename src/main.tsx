4b9ac457e585234fbdd66b8e8b82b94bfd32408f
Bartolo de la Rosa Rivera <157636952+Bdelarosarivera@users.noreply.github.com>
2026-05-12 14:47:46 -0400

    Refactor import statements in main.tsx
    Formatted import statements for consistency.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import * as pdfjs from 'pdfjs-dist';
+// @ts-ignore
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.min.mjs?url';
 
-// Configure PDF.js worker
 pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 createRoot(document.getElementById('root')!).render(
