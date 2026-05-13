bb0c691f5b3255aa6f0228b7a6f0b807bd47cf75
Bartolo de la Rosa Rivera <157636952+Bdelarosarivera@users.noreply.github.com>
2026-05-13 11:41:13 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,4 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/Conversor-PDF_Excel/',
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    },
-  },
-
-  define: {
-    global: 'globalThis',
-  },
-
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
 });
-
