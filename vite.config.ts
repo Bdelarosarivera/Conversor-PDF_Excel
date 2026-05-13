50531d624af034a58d6d0c217734aebcd744b286
Bartolo de la Rosa Rivera <157636952+Bdelarosarivera@users.noreply.github.com>
2026-05-13 09:01:55 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: './', // Use relative base for maximum compatibility
+     base: '/Conversor-PDF_Excel/',
+    
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
