0ff2250dc0f17570d72402ac0822f154baebd714
Bartolo de la Rosa Rivera <157636952+Bdelarosarivera@users.noreply.github.com>
2026-05-13 09:53:59 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// IMPORTANTE:
-// Cambia este nombre EXACTAMENTE por el nombre de tu repositorio en GitHub
-const REPO_NAME = 'Conversor-PDF_Excel';
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  // SOLUCIÓN PARA GITHUB PAGES
-  base: `/${REPO_NAME}/`,
+  // IMPORTANTE
+  base: '/Conversor-PDF_Excel/',
 
   resolve: {
     alias: {
@@ -26,11 +22,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    chunkSizeWarningLimit: 2000,
-  },
-
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
   },
 });
